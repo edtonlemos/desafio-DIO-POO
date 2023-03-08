b792ac9d5a6777cca12565e5474750446e8fd6e6
Edton Lemos <edh.15@hotmail.com>
2023-03-07 21:30:49 -0300

    Commit final

--- src/br/com/dio/desafio/app/Main.java
+++ src/br/com/dio/desafio/app/Main.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio.app;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.net.InetAddress;
@@ -28,10 +30,34 @@ public class Main {
         mJava.setDescricao("A nova mentoria para programadores Java");
         mJava.setData(LocalDate.now());
 
-        System.out.println(cJava.toString() +
-                "\n" +
-                cJavaScript +
-                "\n" +
-                mJava);
+//        System.out.println(cJava.toString() +
+//                "\n" +
+//                cJavaScript +
+//                "\n" +
+//                mJava);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp java developer");
+        bootcamp.getConteudos().add(cJava);
+        bootcamp.getConteudos().add(cJavaScript);
+        bootcamp.getConteudos().add(mJava);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Maria da Silva");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + dev1.getConteudosConcluidos());
+        dev1.progredir();
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+//        Dev dev2 = new Dev();
+//        dev2.setNome("João José");
+//        System.out.println("Conteúdos inscritos: " + dev2.getConteudosInscritos());
+//        System.out.println("Conteúdos concluídos: " + dev2.getConteudosConcluidos());
+
+
     }
 }
